2fd05d132f9edf142f1bff317ea0420c97b21340
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-10-31 11:05:36 -0400

    first

---
+++ game.js
@@ -0,0 +1,13 @@
+const ctx = document.getElementById('main-game-board').getContext('2d'); 
+
+
+function mainLoop(){
+    
+}
+
+document.querySelector('.start-btn').onclick = () =>{
+
+    mainLoop();
+    document.querySelector('.start-screen').style.display = "none";
+
+}
