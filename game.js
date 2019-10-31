73442a73fcd9f09413c1072660ed0303d08c0f9d
--replace-all <ignaciobellomo@gmail.com>
2019-10-31 11:33:37 -0400

    bomber created

--- game.js
+++ game.js
@@ -1,13 +1,48 @@
 const ctx = document.getElementById('main-game-board').getContext('2d'); 
+class Game {
+
+    constructor(){
+        this.playerArr = [];
+    }
+
+    generatePowerUps(){
+
+
+    }
+
+    generateRocks() {
+
+
+    }
+
+    createPlayer(color) {
+        this.playerArr.push(new Bomber(color)); 
+        console.log('player created')
+    }
+
+    createMap() {
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
+let g = new Game();
 
 
 function mainLoop(){
-    
+    ctx.clearRect(0, 0, 800, 600);
+    g.createPlayer('red');
 }
 
-document.querySelector('.start-btn').onclick = () =>{
 
+document.querySelector('#start-game').onclick = () =>{
     mainLoop();
-    document.querySelector('.start-screen').style.display = "none";
-
 }
