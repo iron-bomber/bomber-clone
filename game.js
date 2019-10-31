eba27641876fbb97f5653122e6e0c0a1e86e45ef
--replace-all <ignaciobellomo@gmail.com>
2019-10-31 12:37:28 -0400

    map maker created

--- game.js
+++ game.js
@@ -1,4 +1,22 @@
-const ctx = document.getElementById('main-game-board').getContext('2d'); 
+const ctx = document.getElementById('main-game-board').getContext('2d');
+const bombMap = [
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free'],
+    ['free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free', 'wall', 'free'],
+    ['free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free', 'free']
+];
+
 class Game {
 
     constructor(){
@@ -11,7 +29,11 @@ class Game {
     }
 
     generateRocks() {
-
+        for (let i = 0; i < bombMap.length; i++) {
+            for(let j = 0; j < bombMap.length; j++) {
+                if ((j < 2 || j > 17) && (i === 0 || i === 19)
+            }
+        }
 
     }
 
@@ -21,15 +43,25 @@ class Game {
     }
 
     createMap() {
-
-
+        let xCoord = 0;
+        let yCoord = 0;
+        for(let i = 0; i < bombMap.length; i++) {
+            console.log(xCoord, yCoord);
+            for (let j = 0; j < bombMap.length; j++) {
+                if (bombMap[i][j] === 'free') {
+                    ctx.fillStyle = 'green';
+                    ctx.fillRect(xCoord, yCoord, 50, 50);
+                    xCoord += 50;
+                } else {
+                    ctx.fillStyle = 'black';
+                    ctx.fillRect(xCoord, yCoord, 50, 50);
+                    xCoord += 50;
+                }
+            }
+            yCoord += 50;
+            xCoord = 0;
+        }
     }
-
-
-
-
-
-
 
 }
 
@@ -40,6 +72,7 @@ let g = new Game();
 function mainLoop(){
     ctx.clearRect(0, 0, 800, 600);
     g.createPlayer('red');
+    g.createMap();
 }
 
 
